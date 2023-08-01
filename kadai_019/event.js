b9c0d57d0441944d1868d6e6795d7e331932a576
HI89KURA <137121785+HI89KURA@users.noreply.github.com>
2023-08-01 13:23:03 +0900

    Add files via upload

---
+++ kadai_019/event.js
@@ -0,0 +1,13 @@
+const Btn = document.getElementById('btn');
+const Text = document.getElementById('text');
+ 
+
+Btn.addEventListener('click',()=>{
+
+  Text.textContent ='ボタンをクリックしました';
+
+});
+
+
+
+
